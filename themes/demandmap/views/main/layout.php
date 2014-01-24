9503ff999ef8311aa8ad8eae6d1f5324456644af
Stephan Zeidler <stephanzeidler@web.de>
2014-01-24 06:12:28 +0100

    add frontpage buttons

--- themes/demandmap/views/main/layout.php
+++ themes/demandmap/views/main/layout.php
@@ -83,10 +83,42 @@
 $pageController = new Page_Controller();
 $pageController->index(5);
 print $pageController->template->content;
-
-$pageController->index(6);
-print $pageController->template->content;
 ?>
+<div class="region-content">
+  <div class="wrapper-inner">
+    <div class="content-padding">
+      <div id="frontpage-buttons" class="clearfix">
+        <div class="frontpage-button frontpage-button-about">
+          <div class="frontpage-buttons-inner">
+            <a href="<?php print url::site('page/index/2'); ?>"><img src="/themes/demandmap/images/button-about.png" alt=""></a>
+            <h3><a href="<?php print url::site('page/index/2'); ?>">About</a></h3>
+            <p>Donec dignissim nibh sed velit rhoncus, sit amet aliquet nisl
+              tincidunt. Aliquam adipiscing urna vel dolor porttitor, quis
+              vulputate risus sagittis.</p>
+          </div>
+        </div>
+        <div class="frontpage-button frontpage-button-submit">
+          <div class="frontpage-buttons-inner">
+            <a href="<?php print url::site('reports/submit'); ?>"><img src="/themes/demandmap/images/button-submit.png" alt=""></a>
+            <h3><a href="<?php print url::site('reports/submit'); ?>">Submit</a></h3>
+            <p>Donec dignissim nibh sed velit rhoncus, sit amet aliquet nisl
+              tincidunt. Aliquam adipiscing urna vel dolor porttitor, quis
+              vulputate risus sagittis.</p>
+          </div>
+        </div>
+        <div class="frontpage-button frontpage-button-resources">
+          <div class="frontpage-buttons-inner">
+            <a href="#"><img src="/themes/demandmap/images/button-resources.png" alt=""></a>
+            <h3><a href="#">Resources</a></h3>
+            <p>Donec dignissim nibh sed velit rhoncus, sit amet aliquet nisl
+              tincidunt. Aliquam adipiscing urna vel dolor porttitor, quis
+              vulputate risus sagittis.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <!-- content -->
 <div class="region-content">
   <div class="wrapper-inner">
