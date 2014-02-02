caf680f504b6d2a0162e4c61f5b9a0f44a7cbc5b
Stephan Zeidler <stephanzeidler@web.de>
2014-02-02 10:45:50 +0100

    change facebook box to r0g

--- plugins/demandmapbase/views/blocks/social_media_block.php
+++ plugins/demandmapbase/views/blocks/social_media_block.php
@@ -1,4 +1,4 @@
 <?php blocks::open("social-media");?>
 <?php blocks::title(Kohana::lang('ui_main.social_media_block'));?>
-  <iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2FFacebookDevelopers&amp;width=235&amp;height=295&amp;colorscheme=light&amp;show_faces=false&amp;header=false&amp;stream=true&amp;show_border=true" scrolling="no" frameborder="0" style="background:#fff; border:none; overflow:hidden; width:100%; height:295px;" allowTransparency="true"></iframe>
+  <iframe src="//www.facebook.com/plugins/likebox.php?href=https%3A%2F%2Fwww.facebook.com%2Fpages%2F_r0g-agency-for-open-culture-and-critical-transformation%2F373329419359352&amp;width=235&amp;height=295&amp;colorscheme=light&amp;show_faces=false&amp;header=false&amp;stream=true&amp;show_border=true" scrolling="no" frameborder="0" style="background:#fff; border:none; overflow:hidden; width:100%; height:295px;" allowTransparency="true"></iframe>
 <?php blocks::close();?>
