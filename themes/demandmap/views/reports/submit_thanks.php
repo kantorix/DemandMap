07dcd1d1f2559a9155c89a25fccecd3877cf6e31
Stephan Zeidler <stephanzeidler@web.de>
2014-02-13 21:43:32 +0100

    change submission thanks message

---
+++ themes/demandmap/views/reports/submit_thanks.php
@@ -0,0 +1,17 @@
+<div id="content">
+	<div class="content-bg">
+		<!-- start block -->
+		<div class="big-block">
+			<h1><?php echo Kohana::lang('ui_main.reports_submit_new');?></h1>
+			<!-- green-box -->
+			<div class="green-box">
+				<h3>Your Report has been submitted to our system. It will displayed immediately on our map.</h3>
+
+				<div class="thanks_msg"><a href="<?php echo
+					url::site().'main' ?>">Return to the home page</a><br /><br />
+				</div>
+			</div>
+		</div>
+		<!-- end block -->
+	</div>
+</div>
